82fa96adfb889a2319edbe159aa4158221095fde
Kaylen Travis Pillay <kaylen.pillay@softwareological.com>
2022-10-30 17:35:37 +0200

    Agent List
    1. Added core-api module
    
    2. Added network source

--- feature-agent/src/main/java/co/za/softwareological/kaylen/feature_agent/api/model/request/DTORequestAgentList.kt
+++ feature-agent/src/main/java/co/za/softwareological/kaylen/feature_agent/api/model/request/DTORequestAgentList.kt
@@ -4,6 +4,14 @@ internal data class DTORequestAgentList(
     val language: String = DEFAULT_LANGUAGE,
     val isPlayableCharacter: Boolean = DEFAULT_IS_PLAYABLE_CHARACTER
 ) {
+
+    fun getParamMap(): Map<String, String> {
+        return buildMap {
+            put("language", language)
+            put("isPlayableCharacter", isPlayableCharacter.toString())
+        }
+    }
+
     companion object {
         const val DEFAULT_LANGUAGE: String = "en-US"
         const val DEFAULT_IS_PLAYABLE_CHARACTER: Boolean = true
