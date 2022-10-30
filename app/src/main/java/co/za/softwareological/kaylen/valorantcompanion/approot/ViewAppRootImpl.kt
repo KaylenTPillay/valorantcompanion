0c2799d3eae8bd28a4a4b87c4a75f70980c96fb4
Kaylen Travis Pillay <kaylen.pillay@softwareological.com>
2022-10-30 15:46:02 +0200

    App Root initialisation
    1. Added app root activity for startup

--- app/src/main/java/co/za/softwareological/kaylen/valorantcompanion/approot/ViewAppRootImpl.kt
+++ app/src/main/java/co/za/softwareological/kaylen/valorantcompanion/approot/ViewAppRootImpl.kt
@@ -1,5 +1,12 @@
 package co.za.softwareological.kaylen.valorantcompanion.approot
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ViewAppRootImpl : AppCompatActivity()
+class ViewAppRootImpl : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
